352891294eec481356121eb2f77e1c4464e98ba9
microintel <135629446+microintel@users.noreply.github.com>
2023-08-28 21:55:52 +0530

    Update mprosc.js

--- mprosc.js
+++ mprosc.js
@@ -4,7 +4,8 @@ let aud = new Audio("click.mp3");
 let prx =`<div style="margin-top:100px;">
    <center><a href="https://microintel.github.io/cie/" > <button style="background-color: transparent;color: white;border: 2px solid white;padding: 10px 20px;border-radius: 5px;font-size: 16px;font-family: fantasy;" onclick="mpa()">DIPLOMA CIE BOOK EVALUATOR</button></a>
    <br><br><a href="https://microintel.github.io/Result/" > <button style="background-color: transparent;color: white;border: 2px solid white;padding: 10px 20px;border-radius: 5px;font-size: 16px;font-family: fantasy;" onclick="mpa()">Diploma Result [ch-nagar]</button></a></center></div>
-   <br><center>
+   <br><a href="https://microintel.github.io/Micrography/" > <button style="background-color: transparent;color: white;border: 2px solid white;padding: 10px 20px;border-radius: 5px;font-size: 16px;font-family: fantasy;" onclick="mpa()">Micrography</button></a></center></div>
+   <center>
    <h4 style="color:white;font-family:fantasy;">Preview of the next project</h4>
    <br><video width="100%" height="210px" style="border-radius:70px;border:1px solid brown;" controls>
    <source src="mdisk.mp4" type="video/mp4"></video>
@@ -52,4 +53,4 @@ function con() {let aud = new Audio("click.mp3");aud.play();
     </div>`;
     if (mi) {document.getElementById('par').innerHTML = '';}
     else {document.getElementById('par').innerHTML = cu;}
-}
+   }
