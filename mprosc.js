68c5ae2e5e591d8af73b8f9e5e62de166b7429a1
Microintel <135629446+microintel@users.noreply.github.com>
2024-07-30 12:35:09 +0530

    Update mprosc.js

--- mprosc.js
+++ mprosc.js
@@ -109,6 +109,13 @@ window.location.href="https://microintel.github.io/microintel/";
   <center> <p class="infoOfPro">RoadMap for developers.</p></center>
   </div>
   </a>
+
+  <a href="https://microintel.github.io/Resources/file/coding.html" >
+  <div class="ccd">
+  <img id="img" class="pimg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkT9wI4EXAsVStI-OuOttdTOcmZKDOqf8r2w&usqp=CAU">
+  <center> <p class="infoOfPro">LATEX A Text Formater & Design.</p></center>
+  </div>
+  </a>
   
   
   <a href="https://microintel.github.io/Resources/file/coding.html" >
