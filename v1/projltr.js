07d9ab24ff973936f6a88453a165a72716521fb8
Microintel <135629446+microintel@users.noreply.github.com>
2024-12-13 03:02:57 +0530

    Add files via upload

---
+++ v1/projltr.js
@@ -0,0 +1,25 @@
+let tchi =false;
+let stX =0;
+const swd = window.innerWidth;
+document.addEventListener('touchstart', function(event) {
+   const touch = event.touches[0];
+   tchi = true;
+  stX = touch.clientX;});
+
+document.addEventListener('touchmove', function(event) {
+  if (tchi){
+             const touch = event.touches[0];
+           const dmd = touch.clientX - stX;
+ if (stX < swd / 4 && dmd > swd / 4) {
+location.href="micropro.html";
+   tchi = false;
+ }
+if (stX >= swd /4 && dmd < -swd /4) {
+location.href="news.html";
+  tchi = false;
+ } 
+ }});
+ document.addEventListener('touchend', function() {
+           tchi = false;
+           document.getElementById("u22").style.color="white";
+       });
